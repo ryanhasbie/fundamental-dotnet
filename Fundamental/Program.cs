e9ec87adecd2d3e40c2c2d05c60017520c1a496f
ryanhasbie <ryanhasbie7@gmail.com>
2023-05-28 21:46:03 +0700

    Static method

--- Fundamental/Program.cs
+++ Fundamental/Program.cs
@@ -4,46 +4,28 @@ public class Program
 {
     public static void Main(string[] args)
     {
-
-        // Explicit declaration array
-
-        /* int[] numbers = new int[5];
-        numbers[0] = 11;
-        numbers[1] = 12;
-        numbers[2] = 13;
-        numbers[3] = 14;
-        numbers[4] = 15;
-        foreach (int i in numbers)
+        SayHello();
+        Message("Ryan");
+        Console.WriteLine(Sum(10,10));
+        var anonimous = delegate ()
         {
-            Console.WriteLine(i);
-        }
+            Console.WriteLine();
+        };
+    }
 
-        string[] cities = { "Karawang", "Bogor", "Jakarta" };
-        for (int i = 0; i < cities.Length; i++)
-        {
-            Console.WriteLine($"Cities: {cities[i]}");
-        } */
+    static void SayHello()
+    {
+        Console.WriteLine("Hello, World!");
+    }
 
+    static void Message(string message)
+    {
+        Console.WriteLine($"Good Morning {message}");
+    }
 
-        // Array multidimensional
-        /* 
-            {
-                {1,2}, {3,4}
-            }
-        */
-        int[,] matrixs = new int[2, 2];
-        matrixs[0, 0] = 1;
-        matrixs[0, 1] = 2;
-        matrixs[1, 0] = 3;
-        matrixs[1, 1] = 4;
-
-        for (var i = 0; i < matrixs.GetLength(0); i++)
-        {
-            for (var j = 0; j < matrixs.GetLength(1); j++) 
-            {
-                Console.WriteLine(matrixs[i,j]) ;
-            }
-        }
-     }
+    static int Sum(int x, int y)
+    {
+        return x + y;
+    }
 }
 
