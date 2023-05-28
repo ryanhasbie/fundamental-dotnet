5af0c27698c5cc1c180818063ce5357f44b39850
ryanhasbie <ryanhasbie7@gmail.com>
2023-05-28 07:16:27 +0700

    Array in dotnet

--- Fundamental/Program.cs
+++ Fundamental/Program.cs
@@ -1,21 +1,49 @@
-﻿public class Program
+﻿namespace Fundamental;
+
+public class Program
 {
     public static void Main(string[] args)
     {
-        // String Input
-        // Console.WriteLine("Enter your name: ");
-        // var inputUser = Console.ReadLine();
-        // Console.WriteLine(inputUser);
 
-        // Number Input
-        Console.WriteLine("Enter number 1: ");
-        var number1 = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Enter number2: ");
-        var number2 = Convert.ToInt32(Console.ReadLine());
-        var result = number1 + number2;
-        Console.WriteLine("Result: ");
-        Console.WriteLine(result);
+        // Explicit declaration array
 
-    }
+        /* int[] numbers = new int[5];
+        numbers[0] = 11;
+        numbers[1] = 12;
+        numbers[2] = 13;
+        numbers[3] = 14;
+        numbers[4] = 15;
+        foreach (int i in numbers)
+        {
+            Console.WriteLine(i);
+        }
+
+        string[] cities = { "Karawang", "Bogor", "Jakarta" };
+        for (int i = 0; i < cities.Length; i++)
+        {
+            Console.WriteLine($"Cities: {cities[i]}");
+        } */
+
+
+        // Array multidimensional
+        /* 
+            {
+                {1,2}, {3,4}
+            }
+        */
+        int[,] matrixs = new int[2, 2];
+        matrixs[0, 0] = 1;
+        matrixs[0, 1] = 2;
+        matrixs[1, 0] = 3;
+        matrixs[1, 1] = 4;
+
+        for (var i = 0; i < matrixs.GetLength(0); i++)
+        {
+            for (var j = 0; j < matrixs.GetLength(1); j++) 
+            {
+                Console.WriteLine(matrixs[i,j]) ;
+            }
+        }
+     }
 }
 
